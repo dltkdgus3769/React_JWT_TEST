16fa35638d12194b39dafad5204a986414515ce0
이상현 <tkdgus3769@naver.com>
2025-02-03 13:32:52 +0900

    jwt 작업 중

--- src/main/java/com/busanit501/api5012/config/CustomSecurityConfig.java
+++ src/main/java/com/busanit501/api5012/config/CustomSecurityConfig.java
@@ -1,9 +1,16 @@
 package com.busanit501.api5012.config;
 
+import com.busanit501.api5012.security.APILoginSuccessHandler;
+import com.busanit501.api5012.security.APIUserDetailsService;
+import com.busanit501.api5012.security.filter.APILoginFilter;
+import com.busanit501.api5012.util.JWTUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +18,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Log4j2
 @Configuration
@@ -22,6 +29,9 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class CustomSecurityConfig {
+
+    private final APIUserDetailsService apiUserDetailsService;
+    private final JWTUtil jwtUtil;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -39,6 +49,32 @@ public class CustomSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         log.info("===========config=================");
+
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+
+        authenticationManagerBuilder
+                .userDetailsService(apiUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+
+        // Get AuthenticationManager 세팅1
+        AuthenticationManager authenticationManager =
+                authenticationManagerBuilder.build();
+
+        //반드시 필요 세팅1
+        http.authenticationManager(authenticationManager);
+
+        //APILoginFilter 세팅1
+        APILoginFilter apiLoginFilter = new APILoginFilter("/generateToken");
+        apiLoginFilter.setAuthenticationManager(authenticationManager);
+
+        // APILoginSuccessHandler 생성: 인증 성공 후 처리 로직을 담당
+        APILoginSuccessHandler successHandler = new APILoginSuccessHandler(jwtUtil);
+
+        // SuccessHandler 설정: 로그인 성공 시 APILoginSuccessHandler가 호출되도록 설정
+        apiLoginFilter.setAuthenticationSuccessHandler(successHandler);
+        //APILoginFilter의 위치 조정 세팅1, 사용자 인증 전에 ,
+        http.addFilterBefore(apiLoginFilter, UsernamePasswordAuthenticationFilter.class);
 
         http.csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.disable());
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
